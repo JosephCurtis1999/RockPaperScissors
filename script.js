3ade33452cdab857033ef50cfc8adb5d4de6a57d
Joe Curtis <joecurtis9999@gmail.com>
2021-12-18 13:45:12 +0000

    Added outcomes and comments

--- script.js
+++ script.js
@@ -11,3 +11,40 @@ if (computerChoice < 0.34) {
     computerChoice = "scissors";
 }
 
+// If choice 1 is strictly equal to choice 2 the result is a tie
+var compare = function(choice1, choice2) {
+    if(choice1 === choice2) {
+    return "The result is a tie!";
+}
+// rock beats scissors so return rock wins, if paper is chosen paper beats rock
+if(choice1 === "rock") {
+    if(choice2 === "scissors") {
+        return "rock wins";
+    } else {
+        return "paper wins";
+    }
+}
+// paper beats rock so return paper wins, if scissors is chosen scissors beats paper
+if(choice1 === "paper") {
+    if(choice2 === "rock") {
+        return "paper wins";
+    } else {
+        if(choice2 === "scissors") {
+            return "scissors wins";
+    }
+}
+// rock beats scissors so return rocks wins, if paper is chosen paper beats scissors so scissors wins
+if(choice1 === "scissors") {
+    if(choice2 === "rock") {
+        return "rock wins";
+    } else {
+        if(choice2 === "paper") {
+            return "scissors wins";
+        }
+    }
+}
+}
+};
+console.log("User Choice: " + userChoice);
+console.log("Computer Choice: " + computerChoice);
+compare(userChoice, computerChoice);
