1312419a979587ea2bbaba63fe8d65b07c7024fc
Joe Curtis <joecurtis9999@gmail.com>
2021-12-18 13:35:46 +0000

    Added user prompt

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-var choices = ["rock", "paper", "scissors"];
-
-console.log(choices)
+// Brings up an alert to the user to ask what they choose
+var userChoice = prompt("Do you choose rock, paper or scissors?");
+var computerChoice = Math.random();
